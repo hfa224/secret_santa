e06924142a8c770f339d9b31e060cd4afdc5cb0d
hfa224 <helenffionadams@gmail.com>
2024-08-01 09:28:23 +0100

    start linting

--- secret_santa/__init__.py
+++ secret_santa/__init__.py
@@ -1,27 +1,32 @@
+"""
+Creates and runs the secret santa app
+"""
+
 import os
 
 from flask import Flask
+from . import db, auth, user_page, admin_page
 
 
-def create_app(test_config=None): 
+def create_app(test_config=None):
     """
     Create the flask app.
 
     Keyword arguments:
     test_config -- test config, None by default
     """
-        
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
         print("loading from pyfile")
-        app.config.from_pyfile('config.py', silent=False)
+        app.config.from_pyfile("config.py", silent=False)
         print(app.config)
     else:
         # load the test config if passed in
@@ -34,24 +39,20 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
-    
-    from . import db
+        return "Hello, World!"
+
     db.init_app(app)
-    
-    from . import auth
+
     app.register_blueprint(auth.bp)
-    
-    from . import user_page
+
     app.register_blueprint(user_page.bp)
 
-    from . import admin_page
     app.register_blueprint(admin_page.bp)
-    
+
     # the user_page blueprint does not have a url prefix,
     # so it's the main view
-    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule("/", endpoint="index")
 
     return app
